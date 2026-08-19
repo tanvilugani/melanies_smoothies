be9cad700bb993679ee0a9fe02b05a6f33f9fc5d
Tanvi Lugani <luganitanvi@gmail.com>
2026-08-19 17:33:10 +0700

    Update streamlit_app.py
    Added nutrition table for each fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests  
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -8,7 +9,6 @@ st.write(
   """Choose the fruits you want in your custom Smoothie!
   """
 )
-
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write('Then name on your Smoothie will be:', name_on_order)
@@ -28,9 +28,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
-    st.write(ingredients_string)
-
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+    
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + """','"""+ name_on_order+ """')"""
 
@@ -41,9 +41,3 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
-
-# New section to display smoothifroot nutrition information
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)

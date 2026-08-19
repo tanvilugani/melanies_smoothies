f9bb0fc20f02022f12df915ad24c658faf2a3401
Tanvi Lugani <luganitanvi@gmail.com>
2026-08-19 17:27:11 +0700

    Update streamlit_app.py
    Fixed function for response

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,4 @@ if ingredients_list:
 # New section to display smoothifroot nutrition information
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json)
+st.text(smoothiefroot_response.json())
